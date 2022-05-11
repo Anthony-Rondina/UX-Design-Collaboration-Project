e0560d2582481b3241e3c2668489eed680d6c437
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-11 14:01:09 -0400

    Updated server

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+
+app.use(logger('dev'));
+const app = express();
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+
+
+/* Configure to use port 3001 instead of 3000 during
+ development to avoid collision with React's dev server  */
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
