eec88ad69deec5e512f216adee370b397310ae0c
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-13 13:09:00 -0400

    Created database file

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+  }
+);
+
+const db = mongoose.connection;
+
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
