deced8e6665425b44473aece8e3fbed945c21119
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-14 12:16:05 -0400

    added user Profile Page

--- src/pages/App/Homepage/Homepage.js
+++
@@ -1,3 +0,0 @@
-export default function Homepage(props) {
-    return <h1>This is the Homepage</h1>
-};
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,5 @@
+export default function Homepage(props) {
+    return (
+    <h1>This is the Homepage</h1>
+    )
+};
