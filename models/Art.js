0bb1ecbbe9277a41aa3c2850b30b7df6186e76f4
Luke Fleming <lukefleming@Lukes-MBP.fios-router.home>
2022-05-13 22:59:55 -0400

    Added Art Model

---
+++ models/Art.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const artSchema = new Schema(
+    {
+        type: { type: String, required: true, enum: [WATERCOLOR, CANVAS, QUILLING, ACRYLIC, PAPERART] },
+        nameOfArt: { type: String, required: true },
+        views: Number,
+        artPrompt: { type: String, required: true },
+        isDone: Boolean,
+        supplies: { type: String, required: true },
+        gallery: Array,
+        comments: Array,
+        image: { type: String, required: true },
+        cloudinaryImage: { type: String, required: false }
+
+    },
+    { 
+        timestamps: true 
+    }
+)
+
+module.exports = mongoose.model('Art', artSchema);
