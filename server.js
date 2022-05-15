180ca04ad20cd6f9907620cb2ad5e844a89a3e17
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-15 17:28:36 -0400

    Updated server

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cors = require('cors')
 const app = express();
-const cookieParser = require('cookie-parser')
-
-
-const userCtrl = require('./routes/api/users')
 
 require('dotenv').config();
-require('./config/database');
-
-app.use(cookieParser())
-
 
 app.use(cors());
 app.use(logger('dev'));
@@ -22,15 +14,9 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.use('/api/users', userCtrl );
-
-const ensureLoggedIn = require('./config/ensureLoggedIn');
-
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-
-  
 
 
 /* Configure to use port 3001 instead of 3000 during
