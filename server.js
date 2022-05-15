0dc07a9d03b7ea22dd0d86f33d0d36a614dd286c
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-15 19:07:18 -0400

    updated server.js with db connection

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cors = require('cors')
 const app = express();
-
+const userController = require("./routes/api/users")
+const artController = require("./routes/api/art")
 require('dotenv').config();
-
+require('./config/db');
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use("/api/users", userController)
+app.use("/api/art", artController)
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
