dc307e8a4b96b589721e5977ca27e791bb7fa0dc
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-15 22:45:11 -0400

    updated initial routes in app.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+// import AuthPage from "../AuthPage/AuthPage"
 import { Route, Routes } from 'react-router-dom'
 import UserProfilePage from "../UserProfilePage/UserProfilePage"
+import HomePage from "../Homepage/Homepage"
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Homepage/>}/> */}
+        <Route path="/" element={<HomePage/>}/>
+        {/* <Route path="/login" element={<AuthPage/>}/> */}
         <Route path="/user/:id" element={<UserProfilePage/>}/>
       </Routes>
     </div>
