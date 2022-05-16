20a129081ad652687d279dec75d6f44a24db2954
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-15 22:34:09 -0400

    added initial routes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import UserProfilePage from "../UserProfilePage/UserProfilePage"
 function App() {
   return (
     <div className="App">
+      <Routes>
+        {/* <Route path="/" element={<Homepage/>}/> */}
+        <Route path="/user/:id" element={<UserProfilePage/>}/>
+      </Routes>
     </div>
   );
 }
