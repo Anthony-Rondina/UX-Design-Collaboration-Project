3e2bfb4115f17d45e8cafb6475e6c1eb712153d8
jchung501 <johnchung2011@gmail.com>
2022-05-15 20:28:59 -0500

    Updates made to both LoginForm.jsx and SignUpForm.jsx to allow entering in the input field. Reason was due to no handleChange function being used, handleChange appended to onChange properties

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -11,30 +11,30 @@ state = {
   error: ''
 };
 
-// handleChange = (evt) => {
-//   this.setState({
-//     [evt.target.name]: evt.target.value,
-//     error: ''
-//   });
-// };
+handleChange = (evt) => {
+  this.setState({
+    [evt.target.name]: evt.target.value,
+    error: ''
+  });
+};
 
-// handleSubmit = async (evt) => {
-//   evt.preventDefault();
-//   try {
-//     const formData = {...this.state};
-//     delete formData.confirm;
-//     delete formData.error;
-//     // The promise returned by the signUp service method
-//     // will resolve to the user object included in the
-//     // payload of the JSON Web Token (JWT)
-//     const user = await signUp(formData);
-//     // Baby step
-//     this.props.setUser(user);
-//   } catch {
-//     // An error happened on the server
-//     this.setState({ error: 'Sign Up Failed - Try Again' });
-//   }
-// };
+handleSubmit = async (evt) => {
+  evt.preventDefault();
+  try {
+    const formData = {...this.state};
+    delete formData.confirm;
+    delete formData.error;
+    // The promise returned by the signUp service method
+    // will resolve to the user object included in the
+    // payload of the JSON Web Token (JWT)
+    // const user = await signUp(formData);
+    // Baby step
+    // this.props.setUser(user);
+  } catch {
+    // An error happened on the server
+    this.setState({ error: 'Sign Up Failed - Try Again' });
+  }
+};
 
 // We must override the render method
 // The render method is the equivalent to a function-based component
