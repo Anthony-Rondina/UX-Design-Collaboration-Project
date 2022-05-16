ade5a20658a768d3f8f54e1c20258bc14d6fae60
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-16 10:24:42 -0400

    updating test logs

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -8,7 +8,7 @@ const BASE_URL = '/api/users';
 export async function signUp(userData) {
   // Fetch uses an options object as a second arg to make requests
   // other than basic GET requests, include data, headers, etc.
-  console.log('test3')
+  console.log("userData is ",userData)
   const res = await fetch(BASE_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -16,7 +16,7 @@ export async function signUp(userData) {
     // and assigned to a body property on the options object
     body: JSON.stringify(userData)
   });
-  console.log('test4')
+  console.log("res is", res)
   // Check if request was successful
   if (res.ok) {
     // res.json() will resolve to the JWT
