4ce3a56f4405805cfa7868b8acf83a6ce1d16158
jchung501 <johnchung2011@gmail.com>
2022-05-17 00:47:11 -0500

    gave media Icons there own className 1-3, updated their styling. gave them their own individual styling within module.css

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -20,9 +20,9 @@ export default function AuthPage({ setUser }) {
           <h1>Create an account</h1>
           <h5>Already have an account?<span onClick={() => setShowLogin(!showLogin)}> {showLogin ? 'Sign Up' : 'Sign In'}</span></h5>
           <p>Sign up with social</p>
-          <img className={styles.mediaIcon} alt='social-logo' src={fb}/>
-          <img className={styles.mediaIcon} alt='social-logo' src={goog}/>
-          <img className={styles.mediaIcon} alt='social-logo' src={apple}/>
+          <img className={styles.mediaIcon1} alt='social-logo' src={fb}/>
+          <img className={styles.mediaIcon2} alt='social-logo' src={goog}/>
+          <img className={styles.mediaIcon3} alt='social-logo' src={apple}/>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
       </div>
     </main>
