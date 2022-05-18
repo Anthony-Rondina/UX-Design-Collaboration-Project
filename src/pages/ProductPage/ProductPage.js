9945c431ab9ef38448a17a11d1c82976370131a0
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-05-18 17:32:51 -0400

    updated page

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,5 +1,40 @@
-export default function ProductPage() {
+// import UserProfile from "../../components/ProjectPage/UserProfile"
+// import ArtPrompt from "../../components/ProjectPage/ArtPrompt"
+import Videos from "../../components/ProjectPage/Videos"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+
+export default function ProductPage({chosenWork}) {
+    const id = useParams()
+    const [art,setArt]= useState({})
+    const [refresh, setRefresh] = useState(false)
+    // const getData = (input,inpu2) => {
+    //     (async () => {
+    //         try {
+    //             const response = await axios.get(`/api/art/${input}`)
+    //             console.log("response is",response)
+    //             setArt(response.data)
+    //             // console.log("updated user is",response.data)
+    //             if (response.status === 200) {
+    //                 setRefresh(!refresh)
+    //             } else {
+    //                 console.log('Something went wrong')
+    //             }
+
+    //         } catch (err) {
+    //             console.log(err)
+    //             // console.log(`cards is ${cards}`)
+    //         }
+    //     })()
+    // }
     return(
-        <h1>Product Page</h1>
+        <div>
+            {/* <UserProfile /> */}
+            {/* <ArtPrompt /> */}
+            <Videos />
+        </div>
     )
 }
