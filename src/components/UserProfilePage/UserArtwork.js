47640e39781239016cffce8b8ccc75adda08b7aa
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-17 21:17:28 -0400

    updating profile page

--- src/components/UserProfilePage/UserArtwork.js
+++ src/components/UserProfilePage/UserArtwork.js
@@ -1,14 +1,45 @@
 import styles from "./UPPC.module.css"
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 export default function UserArtwork({user}) {
+    const [updatedUser, setUpdatedUser] =useState({})
+    const [refresh, setRefresh] = useState(false)
+    
+
+
+    const getData = (input) => {
+        (async () => {
+            try {
+                const response = await axios.get(`/api/user/${user._id}`)
+                // console.log(response)
+                setUpdatedUser(response.data.foundUser)
+                // console.log(cards)
+                if (response.status === 200) {
+                    setRefresh(!refresh)
+                } else {
+                    console.log('Something went wrong')
+                }
+
+            } catch (err) {
+                console.log(err)
+                // console.log(`cards is ${cards}`)
+            }
+        })()
+    }
+
+    useEffect(() => {
+        getData()
+    },[])
+
     return (
         <div className={styles.filterResults}>
                 <div className={styles.contentBox}>
-                    <h1>MY ARTWORK</h1>
+                    <h1>{updatedUser.username}</h1>
                     <div>
                         <Link to={`/user/${user._id}/upload`}><img src="https://i.imgur.com/JoSbsoa.png" alt="" className={styles.userUpload} /></Link>
                         {console.log(user.artCollection)}
-                        {/* {user.artCollection.map((art) => {
+                        {/* {updatedUser.artCollection.map((art) => {
                             <img src={art.image} alt="user art" />
                         })} */}
                     </div>
