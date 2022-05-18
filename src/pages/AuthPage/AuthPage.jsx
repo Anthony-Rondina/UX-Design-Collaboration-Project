5289374ea9cc5a07fab3df8ea03e83120b376ffe
jchung501 <johnchung2011@gmail.com>
2022-05-17 21:48:21 -0500

    removed first and last name fields, adding anchor tags and hrefs to each image for media icons, opens a new login page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -11,6 +11,7 @@ export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
   const [showH5, setShowH5] = useState(true);
   const [showH1, setShowH1] = useState(true);
+  const [showP, setShowP] = useState(true);
 
   return (
     <main className={styles.AuthPage}>
@@ -21,10 +22,10 @@ export default function AuthPage({ setUser }) {
       <div className={styles.SplitSide}>
           <h1 onChange={() => setShowH1(!showLogin)}> {showLogin ? 'Sign in' : 'Create an account'}</h1>
           <h5 onChange={() => setShowH5(!showLogin)}> {showLogin ? 'New user?' : 'Already have an account?'}<span onClick={() => setShowLogin(!showLogin)}> {showLogin ? 'Create an account' : 'Sign In'}</span></h5>
-          <p>Sign up with social</p>
-          <img className={styles.mediaIcon1} alt='social-logo' src={fb}/>
-          <img className={styles.mediaIcon2} alt='social-logo' src={goog}/>
-          <img className={styles.mediaIcon3} alt='social-logo' src={apple}/>
+          <p onChange={() => setShowP(!showLogin)}>{showLogin ? 'Sign up with social' : 'Sign in with social'}</p>
+          <a href='https://facebook.com' target='_blank'><img className={styles.mediaIcon1} alt='social-logo' src={fb}/></a>
+          <a href='https://accounts.google.com/' target='_blank'><img className={styles.mediaIcon2} alt='social-logo' src={goog}/></a>
+          <a href='https://apple.com' target='_blank'><img className={styles.mediaIcon3} alt='social-logo' src={apple}/></a>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
       </div>
     </main>
