edb5094261eeb9ed722f8e214f4ed144a0333f2d
jchung501 <johnchung2011@gmail.com>
2022-05-17 22:12:15 -0500

    styling updates made to pass / confirm fields. border styling changes, change order of form to have email first and username second.

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -49,12 +49,12 @@ render() {
               <p>- or -</p>
           <h4>Sign up with email</h4>
         <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input type='text' name='username' value={this.state.username} placeholder='Username' onChange={this.handleChange} required />
-          <input type="email" name="email" value={this.state.email} placeholder='Email' onChange={this.handleChange} required />
+          <input type='email' name='email' value={this.state.username} placeholder='Email' onChange={this.handleChange} required />
+          <input type="text" name="username" value={this.state.email} placeholder='Username' onChange={this.handleChange} required />
           {/* <input className={styles.name} type="firstName" name="firstName" value={this.state.firstName} placeholder='First Name' onChange={this.handleChange} required />
           <input className={styles.name2} type="lastName" name="lastName" value={this.state.lastName} placeholder='Last Name' onChange={this.handleChange} required /> */}
-          <input type="password" name="password" value={this.state.password} placeholder='Password' onChange={this.handleChange} required />
-          <input type="password" name="confirm" value={this.state.confirm} placeholder='Confirm Password' onChange={this.handleChange} required />
+          <input className={styles.pass} type="password" name="password" value={this.state.password} placeholder='Password' onChange={this.handleChange} required />
+          <input className={styles.confirm} type="password" name="confirm" value={this.state.confirm} placeholder='Confirm Password' onChange={this.handleChange} required />
           <button type="submit" disabled={disable}>Sign Up</button>
         </form>
       </div>
