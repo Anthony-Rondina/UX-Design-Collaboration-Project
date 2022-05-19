22d40855da814d712c8c0fa2998fb75ff81491bf
opashaj5 <opashaj5@yahoo.com>
2022-05-18 22:00:45 -0500

    created footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,46 +10,48 @@ export default function Footer() {
             <img src={logo} style={{ width: '8rem' }} id="logo" alt="footer logo"></img>
             <ul>
                 <li>
-                <FontAwesomeIcon icon="fa-brands fa-twitter" />
+                <i class="fa-brands fa-twitter"></i>
                 </li>
                 <li>
-                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+                <i class="fa-brands fa-facebook"></i>
                 </li>
                 <li>
-                <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                <i class="fa-brands fa-instagram"></i>
                 </li>
                 <li>
-                <FontAwesomeIcon icon="fa-brands fa-youtube" />
+                <i class="fa-brands fa-youtube"></i>
                 </li>
             </ul>
         </div>
         <ul>
             <li>
-                <Link to="#" className="footer-link">About Us</Link>
+                <Link to="/" className="footer-link">About Us</Link>
             </li>
             <li>
-                <Link to="#" className="footer-link">Terms of Service</Link>
+                <Link to="/" className="footer-link">Terms of Service</Link>
             </li>
             <li>
-                <Link to="#" className="footer-link">Privacy Policy</Link>
+                <Link to="/" className="footer-link">Privacy Policy</Link>
             </li>
             <li>
-                <Link to="#" className="footer-link">FAQs</Link>
+                <Link to="/" className="footer-link">FAQs</Link>
             </li>
             <li>
-                <Link to="#" className="footer-link">Code of Conduct</Link>
+                <Link to="/" className="footer-link">Code of Conduct</Link>
             </li>
             <li>
-                <Link to="#" className="footer-link">Contact Us</Link>
+                <Link to="/" className="footer-link">Contact Us</Link>
             </li>
         </ul>
-        <div class="footer-search-container">
-            <h6>Subscribe to atelier newsletter</h6>
-            <form>
-                <input type="text" name="search"/>
-            </form>
-        </div>
-        <button className={styles.submitBtn} type="submit">Submit</button>
+        <form className={styles.footerForm} action="" method="get">
+            <div>
+                <label for="email">Subscribe to atelier newsletter</label>
+                <input type="email" name="email" id="email" required/>
+            </div>
+            <div class="form-example">
+                <input className={styles.submitBtn} type="submit" value="Submit"/>
+            </div>
+        </form>
     </footer>
     );
 }
