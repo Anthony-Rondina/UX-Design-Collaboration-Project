4aa3326e2f4057c4c453dd612dbfe4058bf839f2
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-05-21 13:45:41 -0400

    images tiled with margin and route to project image added

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -1,24 +1,25 @@
 import styles from "./ArtistCard.module.css";
 import SaveBtn from "./Save Button.png";
+import { Link } from "react-router-dom";
 
-export default function ArtistCard({ artData }) {
+export default function ArtistCard({ artData, user }) {
     return (
         <div className={styles.artCard}>
-            <img className={styles.artImage} src={artData.image} />
+            <img className={user ? styles.saveBtn : styles.saveBtnHidden} src={SaveBtn} />
+            <Link to={`/art/${artData._id}`}> <img className={styles.artImage} src={artData.image} /> </Link>
             <div className={styles.btTextBox}>
                 {/*justify content spaace between */}
-                <div className={styles.artText}>
+                <div className={styles.artName}>
                     Name of Art
                     {/* <p stye={{ fontWeight: 'bold' }}>{artData.user.firstName} {artData.user.lastName}</p> */}
                     {/* ternary if name length <0 then blah */}
-                    <p className={styles.artTitle}> {artData.title} Artist Name</p>
+                    <p className={styles.artistName}> {artData.title} Artist Name</p>
                 </div>
                 <div className={styles.views}>
                     <p>
                         {artData.views} Views
                     </p>
                 </div>
-                <img src={SaveBtn} />
             </div>
         </div>
     );
