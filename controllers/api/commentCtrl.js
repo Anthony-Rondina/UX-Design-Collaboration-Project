9f252ab41d3d5f9cc4975b4cbf838bbab9cb4155
Luke Fleming <lukefleming@Lukes-MBP.fios-router.home>
2022-05-20 23:59:22 -0400

    Add Comment Route

---
+++ controllers/api/commentCtrl.js
@@ -0,0 +1,26 @@
+const Comment = require('../../models/Comment');
+const Art = require('../../models/Art');
+const User = require('../../models/User');
+
+module.exports = {
+  createComment
+};
+
+async function createComment(req,res) {
+
+    try {
+
+        const { body } = req
+        const user = await User.findById(req.user._id)
+        const art = await Art.findById(req.params.id)
+        const comment = new Comment(body)
+        art.comments.push(comment._id)
+        comment.save()
+        art.save()
+        res.status(200).json({ message: "Worked!" })
+
+    } catch (e) {
+        res.status(400).json(e);
+    }
+
+}
