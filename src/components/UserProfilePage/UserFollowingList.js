e5327cc6639197dba7003e64019654e25ea40cd0
Anthony-Rondina <anthony.rondina@icloud.com>
2022-05-23 10:08:18 -0400

    updated individual following lists

--- src/components/UserProfilePage/UserFollowingList.js
+++ src/components/UserProfilePage/UserFollowingList.js
@@ -8,16 +8,30 @@ export default function UserFollowingList({loggedInUser,followUser, unfollowUser
                     <div className={styles.contentBox}>
                         <h1>Users I'm Following</h1>
                         <div className={styles.followBox}>
-                        {loggedInUser.following.map((followedUser) => {
-                            return(
-                                
-                            <Link to={`/user/${followedUser._id}`}><div>
-                                {console.log(loggedInUser)}
-                            <img src={followedUser.avatar} alt="" style={{ width: 170, height: 170, borderRadius: 90,margin: "5px 20px 3px 20px"}} />
-                            <p>{followedUser.username}</p>
-                        </div></Link>
-                            )
-                        })}  
+                            {loggedInUser._id === id ? 
+                            loggedInUser.following.map((followedUser) => {
+                                return(
+                                    
+                                <Link to={`/user/${followedUser._id}`}><div>
+                                    {console.log(loggedInUser)}
+                                <img src={followedUser.avatar} alt="" style={{ width: 170, height: 170, borderRadius: 90,margin: "5px 20px 3px 20px"}} />
+                                <p>{followedUser.username}</p>
+                            </div></Link>
+                                )
+                            }) 
+                            :
+                            updatedUser.following.map((followedUser) => {
+                                return(
+                                    
+                                <Link to={`/user/${followedUser._id}`}><div>
+                                    {console.log(loggedInUser)}
+                                <img src={followedUser.avatar} alt="" style={{ width: 170, height: 170, borderRadius: 90,margin: "5px 20px 3px 20px"}} />
+                                <p>{followedUser.username}</p>
+                            </div></Link>
+                                )
+                            }) 
+                        }
+                        
                         </div>
                     </div>
             </div>
