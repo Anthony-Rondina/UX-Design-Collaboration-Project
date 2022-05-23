fd87a5d944a5390d9e8d44d4980a8f7ad573bb9f
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-05-23 18:15:57 -0400

    added css to comments

--- src/components/ProductPage/UserProfile.js
+++ src/components/ProductPage/UserProfile.js
@@ -1,24 +1,80 @@
 import styles from './UserProfile.module.css';
+import { useRef,useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
-export default function UserProfile({art}) {
-    const loaded = () => {
-        return (
-            <div className={styles.UserProfile}>
-            <div>
-                <img className={styles.CircleImg} src={art.user.avatar}/>
-            </div>
-            <div className='UserNameText'>
-                <h1>{art.nameOfArt}</h1>
-                <h2>{art.user.username}</h2>
-            </div>
-            </div>
-        )
+export default function Comments({refresh, setRefresh,art, user}) {
+    const text =useRef()
+    const navigate = useNavigate();
+    let token = localStorage.getItem("token")
+
+    const handleSubmit = async (e) => {
+        // e.preventDefault()
+        try {
+            console.log("test")
+            const response = await axios.post(`/api/comments/${art._id}`, {
+                text: text.current.value
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            // setRefresh(!refresh)
+            navigate(`/art/${art._id}`)
+        } catch (err) {
+            console.log(err)
+        }
     }
-
-    const loading = () => {
-        return <h1>Loading</h1>
+    const handleDelete = async (id) => {
+        // e.preventDefault()
+        try {
+            // console.log(token)
+            const response = await axios.delete(`/api/comments/${art._id}`)
+            setRefresh(!refresh)
+            navigate(`/art/${art._id}`)
+        } catch (err) {
+            console.log(err)
+        }
     }
     return (
-     art && art.user ? loaded() : loading()
-    )
+        <div>
+            <div className={styles.formOutterWrapper}>
+                <div className="form-wrapper">
+                    <form onSubmit={handleSubmit}>
+                        <p>Leave a Comment!</p>
+                        <textarea className={styles.commentsbox} placeholder='Enter comment' type="text" ref={text} />
+                        <div className="submit-button">
+                            <input type="submit" value="Add Comment" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            {art.comments ?
+                <div className="commentBlock">
+                    {/* {console.log("comments is",art.comments)} */}
+                    {art.comments.map((comment, idx) => {
+                    // console.log(comment._id)
+                        return (
+                            
+                            <div key={idx} className="comment">
+                                {console.log("comment is",comment)}
+                                <div className="comment-content">
+                                    <p>{comment.text}</p>
+                                </div>
+                                <div className="comment-user">
+                                    <p>{`Posted by: ${comment.user.firstName}`}</p>
+                                    
+                                    {user.email === comment.user.email || user.admin ?
+                                        <button className="delete-button" onClick={() => { handleDelete(comment._id) }}>Delete Comment</button>
+                                        : ''}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            :
+            ""
+            }
+        </div>
+    )   
 }
